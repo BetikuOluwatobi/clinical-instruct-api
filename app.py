5680ae4da9f3b054ef6bb2c10aa1136c173e2715
BetikuOluwatobi <anthonytobynho@gmail.com>
2025-08-02 14:11:53 +0100

    added index.html template file

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route("/instruct")
 def generate():
     text = request.args.get("text")
-    return text
+    text = escape(text)
+    return f"{text}"
